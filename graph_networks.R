5492339de75369e3cef0d33df144a3614ec47e2f
oromiaGodanna <ori.gsero@gmail.com>
2024-05-13 11:25:05 +0200

    Added function to join small world networks

--- graph_networks.R
+++ graph_networks.R
@@ -1,13 +1,14 @@
 library(igraph)
 library(tidyverse)
+library(parallel)
 
 create_small_world_network <- function(N, k, p) {
     "
     Create a small world network using the Watts-Strogatz model
     Args:
-        N: Number of nodes in the network
-        k: Number of nearest neighbors to connect
-        p: Probability of rewiring an edge
+        N: Number of nodes in the network (integer)
+        k: Number of nearest neighbors to connect (integer)
+        p: Probability of rewiring an edge (float)
     Returns:
         igraph object of the small world network
     "
@@ -51,11 +52,61 @@ create_barabasi_albert <- function(N, m) {
     "
     Create a Barabasi-Albert network
     Args:
-        N: Number of nodes in the network
-        m: Number of edges to attach from a new node to existing nodes
+        N: Integer - number of nodes in the network
+        m: Integer - number of edges to attach from a new node to existing nodes
     Returns:
         igraph object of the Barabasi-Albert network
         "
     g <- sample_pa(N, m = m, directed = FALSE, algorithm = "psumtree")
     return(g)
+}
+
+
+create_social_structure <- function(num_networks, num_connections, network_params) {
+    "
+    create a social structure by connecting small world networks with random connections
+    args:
+        num_networks: integer number of small world networks to create
+        num_connections: integer number of random connections to add between networks
+        network_params: list of parameters for each small world network
+    returns: igraph object of the social structure"
+
+    networks <- lapply(seq_along(network_params), function(i) {
+        params <- network_params[[i]]
+        graph <-  create_small_world_network(N = params$N, k = params$k, p = params$p)
+        V(graph)$name <- paste(i, as.character(1:params$N), sep="-")
+        return(graph)
+    })
+
+
+    # make sure all vertices are named 
+    lapply(networks, function(net) {
+        
+        if (any(is.na(V(net)$name))) {
+            stop("Some vertices are not named in a graph.")
+        }
+    })
+
+    # Union the networks with named vertices
+    all_networks <- Reduce(function(x, y) {
+        if (length(unique(c(V(x)$name, V(y)$name))) != length(c(V(x)$name, V(y)$name))) {
+            stop("Vertex name clashes detected, ensure unique naming across all networks.")
+        }
+        graph.union(x, y, byname=TRUE)
+    }, networks)
+    # add edges between random nodes of the networks
+    for (i in 1:num_connections) {
+        repeat {
+            network1 <- sample(networks, 1)[[1]]
+            network2 <- sample(networks, 1)[[1]]
+            if (!identical(network1, network2)) {
+                node1 <- sample(V(network1)$name, 1)
+                node2 <- sample(V(network2)$name, 1)
+                all_networks <- add_edges(all_networks, c(node1, node2))
+                break
+            }
+        }
+    }
+
+    return(all_networks)
 }
