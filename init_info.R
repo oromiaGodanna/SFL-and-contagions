33df2bdb1f385a626e5430dd21877c5b13f9f5ad
oromiaGodanna <ori.gsero@gmail.com>
2024-06-06 10:59:26 +0200

    Introduced type level attributes

--- init_info.R
+++ init_info.R
@@ -33,6 +33,13 @@ initialize_information <- function(num_info, num_types, mean_attractiveness = 5,
     returns:
         info_df: data frame with information items and attributes
     '
+    # init type level attributes
+    type_attributes <- data.frame(
+        Type = 1:num_types,
+        EngagementScore = runif(num_types, 0.5, 1.5),  #random engagement score
+        DecayRate = runif(num_types, 0.01, 0.05)       #random decay rate
+    )
+
     # assign types as numeric values
     types <- sample(1:num_types, num_info, replace = TRUE)
 
@@ -41,20 +48,19 @@ initialize_information <- function(num_info, num_types, mean_attractiveness = 5,
         Type = as.factor(types),
         Attractiveness = numeric(num_info),
         Popularity = numeric(num_info),
-        Novelty = numeric(num_info)  
+        Novelty = numeric(num_info) 
     )
 
     # assign attributes to informations based on type with probabilistic variation
     for (t in 1:num_types) {
-        # indices of items with type t
+        # get indices of items with type t
         indices <- which(info_df$Type == t)
-
-        # base attribute values for the info type
         base_attractiveness <- rtruncnorm(1, a = lower_bound_attractiveness, b = upper_bound_attractiveness,
                                           mean = mean_attractiveness, sd = sd_attractiveness)
         base_popularity <- round(rnorm(1, mean = mean_popularity, sd = sd_popularity))
         base_popularity <- pmax(pmin(base_popularity, upper_bound_popularity), lower_bound_popularity)
-        #base_novelty <- rtruncnorm(1, a = lower_bound_novelty, b = upper_bound_novelty,
+
+       #base_novelty <- rtruncnorm(1, a = lower_bound_novelty, b = upper_bound_novelty,
          #                         mean = mean_novelty, sd = sd_novelty)
 
         # probablity of being simmilar to the base within the type with minimum 40% chance
@@ -64,13 +70,12 @@ initialize_information <- function(num_info, num_types, mean_attractiveness = 5,
         
             #novelity is drawn randomly from a truncated normal distribution for each item
             info_df$Novelty[i] <- rtruncnorm(1, a = lower_bound_novelty, b = upper_bound_novelty,
-                                                 mean = mean_novelty, sd = sd_novelty)
+                                             mean = mean_novelty, sd = sd_novelty)
             if (runif(1) < prob_similar) {
                 # attributes are similar to the base with some variation
                 info_df$Attractiveness[i] <- base_attractiveness + rnorm(1, mean = 0, sd = 0.5)
                 info_df$Popularity[i] <- round(base_popularity + rnorm(1, mean = 0, sd = 5))
                 info_df$Popularity[i] <- pmax(pmin(info_df$Popularity[i], upper_bound_popularity), lower_bound_popularity)
-
             } else {
 
                 # attributes are drawn randomly from a normal distribution
@@ -78,36 +83,46 @@ initialize_information <- function(num_info, num_types, mean_attractiveness = 5,
                                                         mean = mean_attractiveness, sd = sd_attractiveness)
                 info_df$Popularity[i] <- round(rnorm(1, mean = mean_popularity, sd = sd_popularity))
                 info_df$Popularity[i] <- pmax(pmin(info_df$Popularity[i], upper_bound_popularity), lower_bound_popularity)
-                
             }
         }
     }
 
-    return(info_df)
+    # normalize item-level attributes
+    # info_df <- normalize_info_attributes(info_df)
+    return(list(info_df = info_df, type_attributes = type_attributes))
 }
 
-add_new_information <- function(info_df, num_new_info, num_types, mean_attractiveness = 5, sd_attractiveness = 1.5,
+add_new_information <- function(info_df, num_new_info, type_attributes, mean_attractiveness = 5, sd_attractiveness = 1.5,
                                 lower_bound_attractiveness = 0, upper_bound_attractiveness = 10,
-                                lower_bound_popularity = 0, upper_bound_popularity = 10,
-                                lower_bound_novelty = 0, upper_bound_novelty = 10) {
+                                lower_bound_popularity = 1, upper_bound_popularity = 10,
+                                lower_bound_novelty = 0, upper_bound_novelty = 10,
+                                prob_base_on_existing = 0.7) {  #with probablity of basing attractiveness on existing items
 
     new_ids <- (max(info_df$ID) + 1):(max(info_df$ID) + num_new_info)
-    new_types <- sample(1:num_types, num_new_info, replace = TRUE)
-
-    # Generate new attributes
-    new_attractiveness <- rtruncnorm(num_new_info, a = lower_bound_attractiveness, b = upper_bound_attractiveness, mean = mean_attractiveness, sd = sd_attractiveness)
-    new_popularity <- rep(lower_bound_popularity, num_new_info)
-    new_novelty <- rep(upper_bound_novelty, num_new_info)
+    new_types <- sample(1:nrow(type_attributes), num_new_info, replace = TRUE)
 
     new_info_df <- data.frame(
         ID = new_ids,
         Type = as.factor(new_types),
-        Attractiveness = new_attractiveness,
-        Popularity = new_popularity,
-        Novelty = new_novelty
+        Attractiveness = numeric(num_new_info),
+        Popularity = rep(lower_bound_popularity, num_new_info),
+        Novelty = rep(upper_bound_novelty, num_new_info)
     )
 
-    # Combine with existing data
-    # updated_info_df <- rbind(info_df, new_info_df)
+    for (i in 1:num_new_info) {
+        current_type <- new_types[i]
+        existing_items <- info_df[info_df$Type == current_type, ]
+        
+        if (nrow(existing_items) > 0 && runif(1) < prob_base_on_existing) {
+            # base attractiveness on an existing item with some variation
+            base_item <- existing_items[sample(nrow(existing_items), 1), ]
+            new_info_df$Attractiveness[i] <- base_item$Attractiveness + rnorm(1, mean = 0, sd = sd_attractiveness / 2)
+        } else {
+            #assign random attractiveness
+            new_info_df$Attractiveness[i] <- rtruncnorm(1, a = lower_bound_attractiveness, b = upper_bound_attractiveness, 
+                                                        mean = mean_attractiveness, sd = sd_attractiveness)
+        }
+    }
+
     return(new_info_df)
 }
