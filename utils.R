38225b3d40e87b630ed50b3c4a9e754e7c7bcb9e
oromiaGodanna <ori.gsero@gmail.com>
2024-07-10 15:37:58 +0200

    Cleaned some files

--- utils.R
+++ utils.R
@@ -1,5 +1,7 @@
 source("./RL_social_SFL.R")
 source('./diffusion_SFL.R')
+
+
 run_simulation_diffusion <- function(graph, num_runs, N=100, alpha=0.7, beta_mu=0.3, beta_sd=0.1, timesteps=100) {
   output <- lapply(1:num_runs, function(x) Info_diffusion_SFL(N=N, alpha=alpha, beta_mu=beta_mu, beta_sd=beta_sd, graph=graph, timesteps = timesteps))
   mean_correct <- vector("numeric", length = 100)
