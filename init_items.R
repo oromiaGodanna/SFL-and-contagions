b6693edc94f6962c2f197b6b303d597de54d90f0
oromiaGodanna <ori.gsero@gmail.com>
2024-07-10 13:29:51 +0200

    Added functions to initialize new items randomly

--- init_items.R
+++ init_items.R
@@ -4,31 +4,49 @@ generate_attribute <- function(mean = 0.5, sd = 0.15, lower = 0, upper = 1) {
   rtruncnorm(1, a = lower, b = upper, mean = mean, sd = sd)
 }
 
+min_max_normalization <-  function(x) (x - min(x)) / (max(x) - min(x))
 
-clip_item_values <- function (item){
+clip_item_values <- function(item) {
     lapply(item, function(x) pmax(pmin(x, 1), 0))
 }
 
 generate_item <- function() {
-  base_sentiment <- generate_attribute()
-  
-  # return a list of attributes for an item - all values between 1 and 0
-  list(
-    attractiveness = generate_attribute(),
-    popularity = generate_attribute(),
-    novelty = generate_attribute(),
-    positive_sentiment = base_sentiment,
-    negative_sentiment = 1 - base_sentiment,
-    credibility = generate_attribute(),
-    emotional_trigger = generate_attribute()
-  )
+    # generate a random sentiment value between -1 and 1
+    base_sentiment <- runif(1, -1, 1)
+    
+    # normalize sentiment to be between 0 and 1
+    normalized_sentiment = (base_sentiment + 1) / 2
+    
+    # return list of attributes for an item with all values ideally between 0 and 1
+    data.frame(
+        attractiveness = generate_attribute(),
+        popularity = generate_attribute(),
+        novelty = generate_attribute(),
+        sentiment = normalized_sentiment,
+        credibility = generate_attribute(),
+        emotional_trigger = generate_attribute()
+    )
 }
 
 generate_one_item <- function() {
   item <- generate_item()
-  clip_item_values(item)
+  
+  return (item)
 }
 
 generate_multiple_items <- function(n) {
-  replicate(n, generate_one_item())
+    items_list <- lapply(1:n, function(x) generate_item())
+    items_df <- do.call(rbind, items_list)
+    return(items_df)
 }   
+
+
+
+normalize_weights <- function(weight_matrix) {
+  
+  for (i in 1:ncol(weight_matrix)) {
+    weight_matrix[, i] <- min_max_normalization(weight_matrix[i, ], min = 0, max = 1)
+  }
+  return(weight_matrix)
+ 
+}
