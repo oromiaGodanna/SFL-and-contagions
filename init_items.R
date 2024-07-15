ffb550555a03630ece6aff16ab9e56f8042be6f4
oromiaGodanna <ori.gsero@gmail.com>
2024-07-15 10:35:57 +0200

    Removed Sentiment

--- init_items.R
+++ init_items.R
@@ -4,10 +4,19 @@ generate_attribute <- function(mean = 0.5, sd = 0.15, lower = 0, upper = 1) {
   rtruncnorm(1, a = lower, b = upper, mean = mean, sd = sd)
 }
 
+generate_attribute_random <- function() {
+  rand <- runif(1, 0, 1)
+  return (rand)
+}
+
 min_max_normalization <-  function(x) (x - min(x)) / (max(x) - min(x))
 
 clip_item_values <- function(item) {
     lapply(item, function(x) pmax(pmin(x, 1), 0))
+}
+
+quadratic_transform <- function (x){
+  return (x^2)
 }
 
 generate_item <- function() {
@@ -15,16 +24,15 @@ generate_item <- function() {
     base_sentiment <- runif(1, -1, 1)
     
     # normalize sentiment to be between 0 and 1
-    normalized_sentiment = (base_sentiment + 1) / 2
+    # normalized_sentiment = (base_sentiment + 1) / 2
+    # sent_transformed <- quadratic_transform(base_sentiment)
     
     # return list of attributes for an item with all values ideally between 0 and 1
     data.frame(
-        attractiveness = generate_attribute(),
-        popularity = generate_attribute(),
-        novelty = generate_attribute(),
-        sentiment = normalized_sentiment,
-        credibility = generate_attribute(),
-        emotional_trigger = generate_attribute()
+        attractiveness = generate_attribute_random(),
+        popularity = generate_attribute_random(),
+        novelty = generate_attribute_random(),
+        emotional_trigger = generate_attribute_random()
     )
 }
 
@@ -50,8 +58,6 @@ generate_item_from_learned_weights <- function(weights){
     attractiveness = generate_attribute(mean = weights[1], sd = 0.01),
     popularity = generate_attribute(mean = weights[2], sd = 0.01),
     novelty = generate_attribute(mean = weights[3], sd = 0.01),
-    sentiment = generate_attribute(mean = weights[4], sd = 0.01),
-    credibility = generate_attribute(mean = weights[5], sd = 0.01),
     emotional_trigger = generate_attribute(mean = weights[6], sd = 0.01)
   )
   return (item)
